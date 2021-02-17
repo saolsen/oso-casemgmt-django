53a08550d1caba5a2a39b0d861bea5c9c6132e8c
steve olsen <dev@steve.computer>
2021-02-17 13:22:57 -0600

    isolate slow query

---
+++ casemgmt/management/commands/slow_query.py
@@ -0,0 +1,33 @@
+import time
+
+from django.core.management.base import BaseCommand
+
+# Note(steve) I pulled out the query and set this up to just run it. It takes about 0.25 seconds for me.
+# A debug build version of this is taking 1.868 seconds.
+class Command(BaseCommand):
+    help = "Run the slow query"
+
+    def handle(self, *args, **options):
+        from django_oso.oso import Oso
+
+        query = 'alan in casemgmt::User.objects.all() and alan.username = "alan-wkcmp" and resource = r and r matches casemgmt::Document and allow(alan, "view", resource)'
+
+        start = time.perf_counter()
+        result = []
+        try:
+            result = list(Oso.query(query, accept_expression=True))
+        finally:
+            end = time.perf_counter()
+            print(f"query completed in {end - start:0.3f}s")
+        print(result)
+
+        # while True:
+        #     # Run OSO (which runs in loop too), and handle unhandled exceptions (KB Interrupt is already handled within
+        #     # repl as a normal return.
+        #     try:
+
+        #         print(list(Oso.query(query, accept_expression=True)))
+        #         #Oso.repl()
+        #         return
+        #     except Exception as e:
+        #         traceback.print_exc()
